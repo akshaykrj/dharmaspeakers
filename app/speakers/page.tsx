e71761a934c1ced693566712263cfefe7e9dee40
Akshay Jha <akshayjha@Mac.lan>
2026-05-08 18:35:53 +0530

    Make speakers pages dynamic

--- app/speakers/page.tsx
+++ app/speakers/page.tsx
@@ -1,6 +1,8 @@
 import Link from "next/link";
 import { supabase } from "@/lib/supabase";
 
+export const dynamic = "force-dynamic";
+
 export default async function SpeakersPage() {
   const { data: speakers, error } = await supabase
     .from("speakers")
